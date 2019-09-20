4bd023dc20b0e94fe7b0c1bb8bf957289aaa9664
randompiche <antoinepiche@gmail.com>
2019-09-20 13:42:07 +0200

    fix: missing curly braces

--- modules/head.ts
+++ modules/head.ts
@@ -5,7 +5,7 @@ export default {
   title: 'Antoine Piché - FullStack Developer - Paris',
   meta: [
     { charset: 'utf-8' },
-    { name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0',
+    { name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=0, minimum-scale=1.0, maximum-scale=1.0' },
     { hid: 'description', name: 'description', content: 'Antoine Piché is a FullStack Developer (Typescript) and VueJS enthusiast based in Paris' },
   ],
   link: [
