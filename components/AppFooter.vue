ce2b77616b8e335497b62fdf686f9809190404d4
randompiche <antoinepiche@gmail.com>
2019-09-20 13:17:34 +0200

    add: alt & aria-label tags

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -4,7 +4,7 @@
       <div class="row">
         <div class="col-12">
           <div class="contactme">
-            <a v-for="(s, index) in social" :key="index" :href="s.link" target="_blank"><i :class="s.icon" /></a>
+            <a v-for="(s, index) in social" :key="index" :href="s.link" :alt="s.alt" :aria-label="s.alt" target="_blank"><i :class="s.icon" /></a>
           </div>
         </div>
         <div class="col-12 justify-content-center">
@@ -23,12 +23,12 @@ import { Component, Vue } from 'nuxt-property-decorator'
 @Component
 export default class AppFooter extends Vue {
   social: any[] = [
-    { link: 'mailto:antoinepiche@gmail.com', alt: 'Link to my email address',icon: 'fas fa-paper-plane' },
-    { link: 'https://www.linkedin.com/in/antoinepiche/', alt: 'Link to my Linkedin profile',icon: 'fab fa-linkedin-in' },
-    { link: 'https://github.com/RandomPiche', alt: 'Link to my Github profile',icon: 'fab fa-github' },
-    { link: 'https://medium.com/@randompiche', alt: 'Link to my Medium profile',icon: 'fab fa-medium' },
-    { link: 'https://dev.to/randompiche', alt: 'Link to dev.to profile',icon: 'fab fa-dev' },
-    { link: 'https://stackoverflow.com/users/7262921/randompiche?tab=profile', alt: 'Link to my StackOverflow profile',icon: 'fab fa-stack-overflow' },
+    { link: 'mailto:antoinepiche@gmail.com', alt: 'Link to my email address', icon: 'fas fa-paper-plane' },
+    { link: 'https://www.linkedin.com/in/antoinepiche/', alt: 'Link to my Linkedin profile', icon: 'fab fa-linkedin-in' },
+    { link: 'https://github.com/RandomPiche', alt: 'Link to my Github profile', icon: 'fab fa-github' },
+    { link: 'https://medium.com/@randompiche', alt: 'Link to my Medium profile', icon: 'fab fa-medium' },
+    { link: 'https://dev.to/randompiche', alt: 'Link to dev.to profile', icon: 'fab fa-dev' },
+    { link: 'https://stackoverflow.com/users/7262921/randompiche?tab=profile', alt: 'Link to my StackOverflow profile', icon: 'fab fa-stack-overflow' },
   ]
 }
 </script>
--- components/AppWork.vue
+++ components/AppWork.vue
@@ -8,31 +8,28 @@
         <div class="col-12">
           <div class="featured-items">
             <div class="featured-item">
-              <a href="https://la-releve.com/" target="_blank" class="is-link is-link--lrlv"><span class="content">La Relève</span><span class="coming"> // Design UX/UI / Nuxt.js - Vue.js SSR - (Typescript / Vuex / Vue-Router / Axios + others) / Bootstrap 4.1.3 / SCSS / Node.js / Serverless</span></a>
+              <a href="https://la-releve.com/" target="_blank" class="is-link is-link--lrlv" alt="Link to La Relève website" aria-label="Link to La Relève website"><span class="content">La Relève</span><span class="coming"> // Design UX/UI / Nuxt.js - Vue.js SSR - (Typescript / Vuex / Vue-Router / Axios + others) / Bootstrap 4.1.3 / SCSS / Node.js / Serverless</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://lvdf.io/" target="_blank" class="is-link is-link--lvdf"><span class="content">LVDF.io</span><span class="coming"> // Bootstrap 4.alpha-6 SCSS custom build / jQuery 3 / Symfony 3 / AWS EB</span></a>
+              <a href="https://lvdf.io/" target="_blank" class="is-link is-link--lvdf" alt="Link to LVDF website" aria-label="Link to LVDF website"><span class="content">LVDF.io</span><span class="coming"> // Bootstrap 4.alpha-6 SCSS custom build / jQuery 3 / Symfony 3 / AWS EB</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://www.city-drop.com/" target="_blank" class="is-link is-link--citydrop"><span class="content">City Drop</span><span class="coming"> // Bootstrap 4.0.0-beta SCSS custom build / jQuery 3</span></a>
+              <a href="https://www.city-drop.com/" target="_blank" class="is-link is-link--citydrop" alt="Link to City Drop website" aria-label="Link to City Drop website"><span class="content">City Drop</span><span class="coming"> // Bootstrap 4.0.0-beta SCSS custom build / jQuery 3</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://wearebop.fr/" target="_blank" class="is-link is-link--bop"><span class="content">Brewed Outta Paris</span><span class="coming"> // Wordpress - advanced modification of a simple wordpress theme</span></a>
+              <a href="https://wearebop.fr/" target="_blank" class="is-link is-link--bop" alt="Link to Brewed Outta Paris website" aria-label="Link to Brewed Outta Paris website"><span class="content">Brewed Outta Paris</span><span class="coming"> // Wordpress - advanced modification of a simple wordpress theme</span></a>
             </div>
             <div class="featured-item">
-              <a href="javascript:void(0)" target="_blank" class="is-link is-link--alt"><span class="content">Admail</span><span class="coming"> /// Beta ~aug 2018 / Bulma 0.7.1 / PHP homemade MVC framework</span></a>
+              <a href="https://adn-realty.com" target="_blank" class="is-link is-link--adnrealty" alt="Link to ADN Realty website" aria-label="Link to ADN Realty website"><span class="content">ADN Realty</span><span class="coming"> /// Bootstrap 4.1 SCSS custom build / jQuery 3 / PHP homemade MVC framework</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://adn-realty.com" target="_blank" class="is-link is-link--adnrealty"><span class="content">ADN Realty</span><span class="coming"> /// Bootstrap 4.1 SCSS custom build / jQuery 3 / PHP homemade MVC framework</span></a>
+              <a href="https://www.adn-stud.io/" target="_blank" class="is-link is-link--adnstudio" alt="Link to ADN Studio website" aria-label="Link to ADN Studio website"><span class="content">ADN Stud.io</span><span class="coming"> // Bootstrap 4.0.0-beta SCSS custom build / jQuery 3</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://www.adn-stud.io/" target="_blank" class="is-link is-link--adnstudio"><span class="content">ADN Stud.io</span><span class="coming"> // Bootstrap 4.0.0-beta SCSS custom build / jQuery 3</span></a>
+              <a href="https://www.vor-immobilier.fr/" target="_blank" class="is-link is-link--vorimmo" alt="Link to VOR Immobilier website" aria-label="Link to VOR Immobilier website"><span class="content">VOR Immobilier</span><span class="coming"> // Bootstrap 4.1 SCSS custom build / jQuery 3</span></a>
             </div>
             <div class="featured-item">
-              <a href="https://www.vor-immobilier.fr/" target="_blank" class="is-link is-link--vorimmo"><span class="content">VOR Immobilier</span><span class="coming"> // Bootstrap 4.1 SCSS custom build / jQuery 3</span></a>
-            </div>
-            <div class="featured-item">
-              <a href="http://vue.antoine-piche.com/" target="_blank" class="is-link is-link--alt"><span class="content">Custom tech news review tool</span><span class="coming"> // Vue.js (Axios) / Boostrap 3.3.7 / Newsapi.org</span></a>
+              <a href="http://vue.antoine-piche.com/" target="_blank" class="is-link is-link--alt" alt="Link to my custom tech news review tool" aria-label="Link to my custom tech news review tool"><span class="content">Custom tech news review tool</span><span class="coming"> // Vue.js (Axios) / Boostrap 3.3.7 / Newsapi.org</span></a>
             </div>
           </div>
         </div>
